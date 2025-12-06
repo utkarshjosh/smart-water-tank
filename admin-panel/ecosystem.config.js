fb197efa4fa3984ed213d7e5e573caa88e867a12
Utkarsh <utkarsh@mall91.com>
2025-12-06 17:06:33 +0530

    update deployment for site

--- admin-panel/ecosystem.config.js
+++ admin-panel/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     exec_mode: 'fork',
     env: {
       NODE_ENV: 'production',
-      PORT: 3001
+      PORT: 3012
     },
     error_file: './logs/admin-error.log',
     out_file: './logs/admin-out.log',
